74958e51d8f7d480bf050e4171e59c0d958d82d4
wushifu32 <mailtowubo@163.com>
2020-05-29 21:48:12 +0800

    change the admin render

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 from .models import Question, Choice
 
-# Register your models here.
-admin.site.register(Question)
-admin.site.register(Choice)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 0
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,      {'fields': ['q_text']}),
+        ('Data Information',    {'fields': ['publish_date']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('q_text', 'publish_date')
+    list_filter = ['publish_date']
+    search_fields = ['q_text']
+
+
+admin.site.register(Question, QuestionAdmin)
+# The template of admin is located in django/contrib/admin/templates/admin/
+admin.AdminSite.site_header = 'Django Polls'
