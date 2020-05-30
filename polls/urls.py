dd9d55bbda4738748770cc91de37a2c477162768
wushifu32 <mailtowubo@163.com>
2020-05-30 21:50:49 +0800

    redirect favicon.ico

--- polls/urls.py
+++ polls/urls.py
@@ -1,9 +1,14 @@
-from django.urls import path
+from django.urls import path, re_path
+from django.views.generic.base import RedirectView
+
 from . import views
+
+favicon_view = RedirectView.as_view(url='/static/polls/linux.ico', permanent=True)
 
 app_name = 'polls'
 urlpatterns = [
     path('', views.Index.as_view(), name='index'),
+    re_path(r'^favicon\.ico$', favicon_view),
     # 5/
     path('<int:pk>/', views.Details.as_view(), name='details'),
     # 5/vote
